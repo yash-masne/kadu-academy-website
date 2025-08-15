6194a0aaf0baec93f57de5f8c9003759fda18098
Yash <118477362+yash-masne@users.noreply.github.com>
2025-08-16 04:20:07 +0530

    Delete src_BACKUP/src directory

--- src_BACKUP/src/components/MathDisplay.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
-
-const MathDisplay = ({ latex = '', displayMode = false }) => {
-  return displayMode ? <BlockMath math={latex} /> : <InlineMath math={latex} />;
-};
-
-export default MathDisplay;
